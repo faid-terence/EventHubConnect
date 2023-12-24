9d7b2c13a1d29e087c5d4624e398221849657155
Terence Faid JABO <faidterence@outlook.com>
2023-12-24 20:48:23 +0200

    feat(auth): Implement logout option

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,4 +1,4 @@
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -17,6 +17,9 @@ export const Header = () => {
           />
         </Link>
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
           <SignedOut>
             <Button asChild className="roundend-full" size="lg">
               <Link href="/sign-in">Login</Link>
