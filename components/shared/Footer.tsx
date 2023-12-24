22bc6416f5ac40abbbd24fc768208ddf4d130108
Terence Faid JABO <faidterence@outlook.com>
2023-12-24 22:36:13 +0200

    feat(footer): Implemented footer component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="" width={128} height={38} />
+        </Link>
+        <p>2023 Evently. Developed by Terence Faid JABO</p>
+      </div>
+    </footer>
+  );
+};
